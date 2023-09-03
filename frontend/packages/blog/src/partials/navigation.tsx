975c7432aa4151c89cb4fec2758c6002d560d961
Gabe Miller <gmiller2007@gmail.com>
2023-09-03 00:04:24 -0400

    work in progress on navigation elements on mobile viewport

--- frontend/packages/blog/src/partials/navigation.tsx
+++ frontend/packages/blog/src/partials/navigation.tsx
@@ -2,10 +2,16 @@ import * as React from "react"
 
 const Navigation = () => (
     <nav>
+        <div>L. Gabriel Miller</div>
+        <input id="nav-toggle-state" style={{ display: 'none'} as React.CSSProperties} type="checkbox"></input>
+        <label id="nav-toggle" htmlFor="nav-toggle-state" role="button">
+            <svg viewBox="0 0 100 80" width="40" height="40">
+                <rect width="100" height="20"></rect>
+                <rect y="30" width="100" height="20"></rect>
+                <rect y="60" width="100" height="20"></rect>
+            </svg>
+        </label>
         <ul>
-            <li>
-                L. Gabriel Miller
-            </li>
             <li>
                 <a href="/">About</a>
             </li>
