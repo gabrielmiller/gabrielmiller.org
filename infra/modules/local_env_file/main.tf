5f1aa571df67a77962c1937b0541488f465ca2f8
Gabe Miller <gmiller2007@gmail.com>
2024-07-02 22:29:24 -0400

    Adds command to check tls cert for a particular subdomain to simplify checking cert updates Renames environment variables to be more logical based on their usage, as follows:   - APEX_DOMAIN -> APEX_DOMAIN_ORIGIN   - APEX_BUCKET_NAME -> APEX_DOMAIN Alphebetizes non-source properties when initializing terraform modules

--- infra/modules/local_env_file/main.tf
+++ infra/modules/local_env_file/main.tf
@@ -2,8 +2,8 @@ resource "local_file" "environment" {
   content = <<-ENVFILE
   ALBUM_BUCKET="${var.album_bucket_name}"
   ALBUM_BUCKET_REGION="${var.album_bucket_region}"
-  APEX_BUCKET_NAME="${var.apex_bucket_name}"
-  APEX_DOMAIN="https://${var.apex_domain}"
+  APEX_DOMAIN="${var.apex_domain}"
+  APEX_DOMAIN_ORIGIN="https://${var.apex_domain}"
   AWS_PROFILE="${var.aws_profile}"
   CLOUDFRONT_CACHE_MAX_AGE="${var.cloudfront_cache_max_age}"
   ENVFILE
